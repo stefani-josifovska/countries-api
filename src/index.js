49e9881ea5b801a954dffa2bbcad1a7019997c4f
stefijosi <stefani.josifovska99@gmail.com>
2022-07-19 19:01:25 +0200

    Switched from BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <ModeProvider> */}
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
     {/* </ModeProvider> */}
   </React.StrictMode>
 );
